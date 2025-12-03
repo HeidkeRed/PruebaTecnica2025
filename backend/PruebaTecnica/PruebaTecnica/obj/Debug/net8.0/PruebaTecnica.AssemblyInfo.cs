8adc03224a0aba0d5a0c140de25d8a8b6c6528ba
HeidkeRed <cebg_slp@hotmail.com>
2025-12-02 23:07:21 -0600

    Commit de readme y Documento de Justificacion

--- backend/PruebaTecnica/PruebaTecnica/obj/Debug/net8.0/PruebaTecnica.AssemblyInfo.cs
+++ backend/PruebaTecnica/PruebaTecnica/obj/Debug/net8.0/PruebaTecnica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PruebaTecnica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d07b19da20e7d0dd801c33dcd94a3721699ffa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PruebaTecnica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PruebaTecnica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
